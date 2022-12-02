2bdfaf8c2387086d987e7133f2559e5bbd85599d
Maliheh Zarei Chitgar <108247525+Malihehpooyan@users.noreply.github.com>
2022-12-02 11:54:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 function getCatImage() {
-    const image = new Image(400)
-    document.body.appendChild(image)
+  const image = new Image(400);
+  document.body.appendChild(image);
   fetch("https://api.thecatapi.com/v1/images/search")
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       console.log(data[0].url);
-       document.getElementById("catImage").src = data[0].url;      
-      image.src  = data[0].url;
+      document.getElementById("catImage").src = data[0].url;
+      image.src = data[0].url;
     })
 
-    .catch(error => console.error(error));
+    .catch((error) => console.error(error));
 }
 getCatImage();
 
-
-function Cats(){
-    getCatImage()
-    const image = document.createElement('img')
-    image.src  = "https://api.thecatapi.com/v1/images/search"
+function Cats() {
+  const image = document.createElement("img");
+  image.src = "https://api.thecatapi.com/v1/images/search";
 }
-
