3f1ab5f80b41c66a86abced59af72ba63f0880fe
Maliheh Zarei Chitgar <108247525+Malihehpooyan@users.noreply.github.com>
2022-12-01 21:27:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 function getCatImage() {
-    fetch("https://api.thecatapi.com/v1/images/search")
-        .then((response) => 
-            // Resolve the promise we got from calling then() to the promise that we get from calling json()
-          response.json())
-        .then((data) => {
-            console.log(data[0].url);
-            document.getElementById("catImage").src = data[0].url;
-        })
-       // .catch(error => console.error('Error:', error));//
-     .catch(error => console.error(error));
+    const image = new Image(400)
+    document.body.appendChild(image)
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data[0].url);   
+      image.src  = data[0].url;
+    })
+
+    .catch(error => console.error(error));
 }
 getCatImage();
+
+
+function Cats(){
+    getCatImage()
+    const image = document.createElement('img')
+    image.src  = "https://api.thecatapi.com/v1/images/search"
+}
